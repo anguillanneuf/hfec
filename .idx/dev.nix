1dcfaff2fc4efa72cb496fc42ed1c59838fe0718
branbaratheon.581486 <branbaratheon.581486@gmail.com>
2025-10-06 17:57:14 +0000

    local web preview

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -6,7 +6,7 @@
   # Use https://search.nixos.org/packages to find packages
   packages = [
     # pkgs.go
-    # pkgs.python311
+    pkgs.python3
     # pkgs.python311Packages.pip
     # pkgs.nodejs_20
     # pkgs.nodePackages.nodemon
@@ -23,16 +23,10 @@
     previews = {
       enable = true;
       previews = {
-        # web = {
-        #   # Example: run "npm run dev" with PORT set to IDX's defined port for previews,
-        #   # and show it in IDX's web preview panel
-        #   command = ["npm" "run" "dev"];
-        #   manager = "web";
-        #   env = {
-        #     # Environment variables to set for your server
-        #     PORT = "$PORT";
-        #   };
-        # };
+        web = {
+          command = ["python" "-m" "http.server" "$PORT"];
+          manager = "web";
+        };
       };
     };
     # Workspace lifecycle hooks
